dce7b6680cd9ed36c4a61f360b3f425c940420d9
AlexProgrammerDE <40795980+AlexProgrammerDE@users.noreply.github.com>
2020-05-26 10:19:21 +0200

    More features!

---
+++ data/commands.js
@@ -0,0 +1,21 @@
+module.exports = {
+  bc: [
+  'Use "_coords" to get my coordinates!', 
+  'Use "_tps" to see the tps!', 
+  'Use "_come username" to tell me to which player, which is in my range i should navigate to!', 
+  'Use "_stop" to stop my current navigation!', 
+  'Use "_ping username" or just "_ping" to get the ping of yourself or other players!', 
+  'Use "_goto x y z" to tell me where i should navigate to!', 
+  'Use "_help" to get a list of all my commands!', 
+  'Use "_tpa username" to send a tpa request to a specific player! (I need to be 1000 blocks away from spawn to do this.)', 
+  'Use "_tpy username" to let me accept the tpa request of a player!', 
+  'Use "_rules" to see the rules of the server!', 
+  'Use "_no" to let me disagree!',
+  'Use "_yes" to let me agree!',
+  'Use "_no" to let me disagree!',
+  'Use "_report username reason" to report a player!',
+  'Use "_bible" or "_verse" to get a line from the bible!',
+  'Use "_about" to get some information about the bot!',
+  'Please give me some armor! I can automatic wear them and switch to better.'
+  ]
+};
--- index.js
+++ index.js
@@ -3,6 +3,9 @@ const config = require('./config.json');
 var tpsPlugin = require('mineflayer-tps')(mineflayer);
 var Vec3 = require('vec3').Vec3;
 const navigatePlugin = require('mineflayer-navigate')(mineflayer);
+const bible = require('./data/bible');
+const commands = require('./data/commands');
+var mirror = false;
 
 var bot = mineflayer.createBot({
   host: config.host,
@@ -22,19 +25,21 @@ bot.loadPlugin(tpsPlugin);
 bot.on('chat', function(username, message) {
   console.log(message);
   
+  if (username === undefined) return;
+  
   if (username === bot.username) return;
   
-  if (message == '|coords') {
+  if (message == '_coords') {
 	var pos = Number.parseInt(bot.entity.position.x);
 	
 	bot.chat("My coords are: " + Number.parseInt(bot.entity.position.x) + " " + Number.parseInt(bot.entity.position.y) + " " + Number.parseInt(bot.entity.position.z));
   }
   
-  if (message == '|tps') {
+  if (message == '_tps') {
     bot.chat('Current tps: ' + bot.getTps())
   }
   
-  if (message === '|come') {
+  if (message === '_come') {
 	if (bot.players[username].entity == null) {
 		bot.chat(username + " it seems like your out of range.");
 	} else {
@@ -44,15 +49,42 @@ bot.on('chat', function(username, message) {
 	}
   }
   
-  if (message === '|stop') {
+  if (message === '_stop') {
     bot.navigate.stop();
   }
   
-  if (message === '|ping') {
-	bot.chat(username + " your ping is: " + bot.players[username].ping);
+  if (message.startsWith('_ping')) {
+	if (message === '_ping') {
+	  if (bot.players[username]) {
+	    bot.chat(username + " your ping is: " + bot.players[username].ping);
+	  } else {
+		bot.chat("Sorry i can only ping players.");  
+	  }
+    } else {
+	  var messages = message.split(" ");
+	  
+	  if (messages.length === 2) {
+	  	if (bot.players[messages[1]]) {
+	      if (messages[1] === username) {
+		    if (bot.players[username]) {
+	           bot.chat(username + " your ping is: " + bot.players[username].ping);
+	        } else {
+			  bot.chat("Sorry i can only ping players.");    
+			}
+		  } else {
+		   	bot.chat("The ping of " + bot.players[messages[1]].username + " is: " + bot.players[messages[1]].ping);
+		  }
+		} else {
+	      bot.chat("Sorry i can't find that player.");  
+		}
+		
+	  } else {
+		bot.chat("Sorry you didnt use: _ping username")  
+	  }
+	}
   }
   
-  if (message.startsWith('|goto')) {
+  if (message.startsWith('_goto')) {
 	var messages = message.split(" ");
 	
 	if (messages.length === 4) {
@@ -62,29 +94,83 @@ bot.on('chat', function(username, message) {
 		
 		bot.navigate.to(targetpos);
 	} else {
-		bot.chat("Sorry you didnt use: |goto x y z");
+		bot.chat("Sorry you didnt use: _goto x y z");
 	}
   }
   
-  if (message === '|help') {
-	bot.chat("PistonBot help: |tpa, |ping, |goto, |come, |stop, |coords");
+  if (message === '_help') {
+	bot.chat("PistonBot help: _tps, _ping, _goto, _come, _stop, _coords, _tpa, _tpy, _rules, _report, _bible, _about");
   }
   
-  if (message.startsWith('|link')) {
-	var messages = message.split(" ");
+  if (message.startsWith('_tpa')) {
+    var messages = message.split(" ");
 	
 	if (messages.length === 2) {
-		if (messages[1] === 'test') {
-			bot.chat("https://google.com");
-		}
+		bot.chat("/tpa " + messages[1]);
 	} else {
-		bot.chat("Sorry you should use: |link topic");
+		bot.chat("Sorry you should use: _tpa username");
 	}
+  }
+  
+  if (message.startsWith('_tpy')) {
+    var messages = message.split(" ");
+	
+	if (messages.length === 2) {
+		bot.chat("/tpy " + messages[1]);
+	} else {
+		bot.chat("Sorry you should use: _tpy username");
+	}
+  }
+  
+  if (message === '_rules') {
+    bot.chat("No rules, but pls don't spam, hack, dupe, xray, swear or grief.");
+  }
+  
+    if (message === '_no') {
+    bot.chat("NO!");
+  }
+  
+  if (message === '_yes') {
+    bot.chat("YES!");
+  }
+  
+  if (message.startsWith('_report')) {
+    var messages = message.split(" ");
+	
+	if (messages.length >= 3) {
+		bot.chat("Reported " + messages[1] + " for " + message.replace("_report " + messages[1], ""));
+	} else {
+		bot.chat("Sorry you should use: _report username reason");
+	}
+  }
+  
+  if (message === '_bible' || message === '_verse') {
+    bot.chat(bible.proverbs[Math.round(Math.random() * 27)]);
+  }
+  
+  if (message === '_about') {
+    bot.chat("PistonBot coded by Pistonmaster with <3.");
+  }
+  
+  if (message.startsWith('_say') && username === 'Pistonmaster') {
+	var say = message.replace("_say ", "");
+	
+	bot.chat(say);
+  }
+  
+  if (message === '_killbot' && username === 'Pistonmaster') {	
+	bot.chat("/kill");
+  }
+  
+  if (message === 'test') {
+    console.log(bot.inventory.items());
   }
 });
 
-bot.on('spawn', function() {
+bot.on('login', function() {
   bot.chat("/login " + config.ingamepassword);
+  
+  console.log("I spawned and set everything up.");
 });
 
 bot.navigate.on('pathFound', function (path) {
@@ -104,4 +190,45 @@ bot.navigate.on('interrupted', function() {
   bot.chat("stopping");
 });
 
+let interval = setInterval(() => {
+  bot.chat(commands.bc[Math.round(Math.random() * 16)]);
+}, 60000)
+
+bot.on('kicked', function(reason) {
+  console.log("I got kicked for " + reason);
+});
+
+bot.on('time', function() {
+  /* if (bot.food != 20) {
+	
+  }
+  bot.consume(err => console.log(err)) */
+	
+  var entity = nearestEntity();
+  if(entity) {
+	if (entity.type === 'player') {
+      bot.lookAt(entity.position.offset(0, 1, 0));
+	} else if (entity.type === 'mob') {
+	  bot.lookAt(entity.position);
+	}
+  }
+});
+
 bot.on('error', err => console.log(err));
+
+function nearestEntity(type) {
+  var id, entity, dist;
+  var best = null;
+  var bestDistance = null;
+  for(id in bot.entities) {
+    entity = bot.entities[id];
+    if(type && entity.type !== type) continue;
+    if(entity === bot.entity) continue;
+    dist = bot.entity.position.distanceTo(entity.position);
+    if(!best || dist < bestDistance) {
+      best = entity;
+      bestDistance = dist;
+    }
+  }
+  return best;
+}
